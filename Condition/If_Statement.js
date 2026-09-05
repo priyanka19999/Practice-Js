ad804dd00a77e6cbc9cc08c50453f047f4d6ecd4
Priyanka Priyadarshani <137867082+priyanka19999@users.noreply.github.com>
2026-09-05 23:43:26 +0530

    JS If Conditions
    JS If Conditions

---
+++ Condition/If_Statement.js
@@ -0,0 +1,18 @@
+// Use the JavaScript if statement to execute a block of code when a condition is true.
+
+// Syntax
+// if (condition) {
+//   //  block of code to be executed if the condition is true
+// }
+
+
+// Example-1
+let age = 18;
+let text = "You can Not drive";
+if (age >= 18) {
+    text = "You can drive";
+}
+console.log(age);
+console.log(text);
+// 18
+// You can drive
