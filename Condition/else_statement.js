9b92dc63982599699347cd653af7f203030a16eb
Priyanka Priyadarshani <137867082+priyanka19999@users.noreply.github.com>
2026-09-07 00:02:29 +0530

    else statement
    else statement

---
+++ Condition/else_statement.js
@@ -0,0 +1,12 @@
+// Use the else statement to specify a block of code to be executed if a condition is false.
+
+let hour = 20;
+let greeting;
+
+if (hour < 18) {
+    greeting = "Good day";
+} else {
+    greeting = "Good evening";
+}
+
+console.log(greeting);
