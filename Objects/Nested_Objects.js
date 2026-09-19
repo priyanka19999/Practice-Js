d0292c123140a8e2d56db5746c0f1b5f09cfc215
Priyanka Priyadarshani <137867082+priyanka19999@users.noreply.github.com>
2026-09-19 21:15:22 +0530

    Nested Objects
    Nested Objects

---
+++ Objects/Nested_Objects.js
@@ -0,0 +1,12 @@
+// Nested Objects
+let person2 = {
+    name: "Priyanka",
+
+    address: {
+        city: "Bhubaneswar",
+        state: "Odisha",
+        country: "India"
+    }
+};
+console.log(person2.address.city); //Bhubaneswar
+console.log(person2.address.country); //India
