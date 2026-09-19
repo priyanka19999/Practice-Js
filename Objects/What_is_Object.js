c55488f14f08a37b2c2ae7d695dd3c21ceba2aa9
Priyanka Priyadarshani <137867082+priyanka19999@users.noreply.github.com>
2026-09-19 21:05:48 +0530

    What is an Object?
    What is an Object?

---
+++ Objects/What_is_Object.js
@@ -0,0 +1,24 @@
+// Think of an object as a container that stores related information together.
+
+// Syntax:
+
+// let objectName = {
+//     key: value,
+//     key: value,
+//     key: value
+// };
+
+
+// What is Object ?
+// An object stores data in key-value pairs.
+let person = {
+    name: "Priyanka",
+    age: 25,
+    city: "Bhubaneswar"
+};
+console.log(person);
+
+
+
+
+
