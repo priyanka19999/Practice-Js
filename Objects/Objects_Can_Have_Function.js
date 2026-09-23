7bcc478897047e968b3e420b35db9e61a10c616b
Priyanka Priyadarshani <137867082+priyanka19999@users.noreply.github.com>
2026-09-23 21:17:19 +0530

    Function inside an object is called a method
    Function inside an object is called a method

---
+++ Objects/Objects_Can_Have_Function.js
@@ -0,0 +1,10 @@
+// A function inside an object is called a method.
+let person = {
+    name: "Rahul",
+
+    greet: function() {  //is a method.
+        console.log("Hello!");
+    }
+};
+
+person.greet(); //Hello!
